7623b6add48dd4ae2482e0b50c1da4e5999737c9
nevillecarvalho <neville.carvalho@datoscloud.io>
2014-06-24 16:38:18 -0700

    Fix last put

---
+++ third-party/rabbitmq/helloworld_send.py
@@ -0,0 +1,21 @@
+import pika
+
+import logging
+
+
+logging.getLogger('pika').setLevel(logging.CRITICAL)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+
+print " [x] Sent 'Hello World!'"
+
+
+connection.close()

bce58f89b5164c0b138212f3bef497ad5eafb314
Maohua Lu <maohua.lu@datoscloud.io>
2014-06-25 14:16:26 -0700

    BID-1 #comment Support manage and store path for Policy Engien

--- src/store/policy_engine/main.py
+++ src/store/policy_engine/main.py
@@ -10,6 +10,8 @@ import datos_constants as DC
 
 my_local_policy_store = local_pe_backend()
 my_local_mapping_store = local_pe_backend('FileRelations.txt')
+my_local_l2p_store = local_pe_backend('FileToObjects.txt')
+
 
 channel = None
 global_policy_engine_queue = DC.POLICY_ENGINE_QUEUE_NAME
@@ -35,9 +37,11 @@ def handle_delivery(ch, method, header, body):
     print " [x] Received %r" % (body,)
     cmdComps = body.split(',')
     
-    if len(cmdComps) != 3:
+    if len(cmdComps) < 3:
         #TODO: Emit the ERROR entry to Error logging
-        print "[ERROR] Failed to get the full command. \n The full command should be operation: type, filename: name, interval: seconds"
+        print "[ERROR] Failed to get the full command. "
+        print "The full command should be operation: type, filename: name, interval: seconds[,dest_name:name,status:init/done]"
+        
         return
     else:
         operationPair = cmdComps[0].split(':')
@@ -46,7 +50,8 @@ def handle_delivery(ch, method, header, body):
 
     	if len(intervalPair) != 2 or len(operationPair) != 2 or len(namePair) != 2:
             #TODO: Ack the failure
-            print "[ERROR] Failed to get the full command. \n The full command should be operation: type, filename: name, interval: seconds"
+            print "[ERROR] Failed to get the full command."
+            print "The full command should be operation: type, filename: name, interval: seconds[,dest_name:name,status:init/done]"
             ch.basic_ack(delivery_tag = method.delivery_tag)
             
             return
@@ -57,21 +62,40 @@ def handle_delivery(ch, method, header, body):
             interval = intervalPair[1].strip()
             
             if operation == 'manage':
-    			#TODO: Store the policy in Swift
+                #TODO: Store the policy in Swift
                 print "manage function"
                 
                 my_local_policy_store.put(filename, interval)
                 
-                #TODO: for Hive, need to query the Hive metastore
+                #TODO: for hive, need to query Hive metastore
                 my_local_mapping_store.put(filename, filename)
+                
                 
                 node_agent_channel.basic_publish(exchange='',
                           routing_key=global_node_agent_queue,
                           body=body)
                 
             elif operation == 'store':
-    			# TODO: Store the data, implement it later
-                print "Start to store data at interval " + interval
+    			
+                if len(cmdComps) != 5: 
+                    print "Wrong format, should be operation:type,filename:name,interval: seconds,dest_name:name,status:init/done"
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+                
+                dest_name_pair = cmdComps[3].split(':')
+                status_pair = cmdComps[4].split(':')
+                
+                if len(dest_name_pair) != 2 or len(status_pair) != 2: 
+                    print "Wrong format, should be operation:type,filename:name,interval: seconds,dest_name:name,status:init/done"
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+                
+                dest_name = dest_name_pair[1].strip()
+                status_value = status_pair[1].strip()
+                
+                print "Start to store data at timestamp " + interval
                 storeInterval, storeStatus = my_local_policy_store.get(filename)
                 
                 if not storeStatus: 
@@ -82,11 +106,17 @@ def handle_delivery(ch, method, header, body):
                     return
                 
                 
+                my_local_l2p_store.put(str(filename), str(dest_name) + ':' + str(status_value) + ":" + str(interval))
                 
                 
             elif operation == 'retrieve':
     			# TODO: retrieve the data
                 print "retrieve function"       
+                if len(cmdComps) != 4: 
+                    print "Wrong format, should be operation:type,filename:name,interval: seconds,dest_dir:dirname"
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
                 
                 storeInterval, storeStatus = my_local_policy_store.get(filename)
                 
@@ -97,7 +127,60 @@ def handle_delivery(ch, method, header, body):
             
                     return
                 
-                print "Stored interval is " + str(storeInterval)
+                print "Retrieve high-level mapping for " + str(filename) + " at " + str(interval)
+                
+                storeMappedFile, storeStatus = my_local_mapping_store.get(filename)
+                if not storeStatus: 
+                    # TODO: Raise error
+                    print "Cannot find the high-level mapping for " + str(filename)
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+            
+                print "Retrieve object for " + str(storeMappedFile) + " at " + str(interval)
+                
+                storeEntry, storeStatus = my_local_l2p_store.get(str(storeMappedFile))
+                if not storeStatus: 
+                    # TODO: Raise error
+                    print "Cannot find the store object for " + str(filename)
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+               
+                print "Store entry is " + str(storeEntry)
+                
+                storeComp = storeEntry.split(":")
+                
+                if len(storeComp) != 3:
+                    # TODO: Raise error
+                    print "The metadata for " + str(filename) + " is corrupted"
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+                    
+                if storeComp[2] != str(interval) : 
+                    print "Metadata for " + str(filename) + " does not contain entry for timestamp " + str(interval)
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+            
+                if storeComp[1] != 'done' : 
+                    print "Metadata for " + str(filename) + " is inited, but not closed "
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+                
+                dest_name_pair = cmdComps[3].split(':')
+                
+                if len(dest_name_pair) != 2: 
+                    print "Wrong format, should be operation:type,filename:name,interval: seconds,dest_dir:dirname"
+                    ch.basic_ack(delivery_tag = method.delivery_tag)
+            
+                    return
+                
+                #TODO: start the transfer of the file back to HDFS
+                dest_dir = dest_name_pair[1]
+                print "Going to transfer data back to " + dest_dir
                 
             else: 
     			# TODO: Raise error in error logging
